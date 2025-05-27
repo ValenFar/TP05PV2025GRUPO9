62e99272ee73f9b35af484b99178c51afb099371
unknown <mjorgeluis785@gmail.com>
2025-05-26 21:45:34 -0300

    Agregado los componentes react bootstrap a AddAlumno.jsx, ListaAlumnos.jsx, agregué al array datos aleatorios y temporales para tener referencia en App.jsx y agregué algunos estilos en index.css - Marino Jorge Luis

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import AppRouter from './router/AppRouter.jsx'
 import { useState } from 'react';
 
 function App() {
-  const [alumnos, setAlumnos] = useState([]);
+  const [alumnos, setAlumnos] = useState([{ lu: "123456", nombre: "Juan", apellido: "Pérez", curso: "Ingeniería", telefono: "1234567890", mail: "juangP3@example.com", domicilio: "Calle Falsa 123" }, { lu: "654321", nombre: "Ana", apellido: "Gómez", curso: "Arquitectura", telefono: "0987654321", mail: "AnaG@example.com", domicilio: "Avenida Siempre Viva 456" }, { lu: "789012", nombre: "Luis", apellido: "Martínez", curso: "Medicina", telefono: "1122334455", mail: "martZ@example.com", domicilio: "Calle del Sol 789" }]);
 
   const agregarAlumno = (alumno) => {
     setAlumnos([...alumnos, alumno]);
