de8c90c21861620697bf10ebeee80bd168340419
ValenFar <maxy.far@gmail.com>
2025-06-02 02:45:38 -0300

    Se quito css por completo, se agregaron las funcionalidades para editar y borrar, se aplicaron condiciones al formulario de cargar alumnos y se arreglo la NavBar

--- src/components/AddAlumno.jsx
+++ src/components/AddAlumno.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 //import '../css/Add.css'
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
@@ -7,8 +7,9 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-const AddAlumno = ({ onAdd }) => {
-  const [form, setForm] = useState({
+const AddAlumno = ({ onAdd, onEdit, modo = "agregar", alumno, onClose }) => {
+  const [form, setForm] = useState(
+    alumno || {
     lu: "",
     nombre: "",
     apellido: "",
@@ -18,16 +19,23 @@ const AddAlumno = ({ onAdd }) => {
     domicilio: "",
   });
 
+useEffect(() => {
+    if (alumno) {
+      setForm(alumno);
+    }
+  }, [alumno]);
+
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onAdd(form); // llama a la funcion por proms
-    console.log(form)
-    console.log("el formulario se envio")
-    setForm({
+    if (modo === "editar"){
+      onEdit(form);
+      alert(`Alumno '${form.nombre} ${form.apellido}' Editado Exitosamente!!`);
+      if (onClose) onClose();
+      setForm({ 
       lu: "",
       nombre: "",
       apellido: "",
@@ -35,14 +43,36 @@ const AddAlumno = ({ onAdd }) => {
       telefono: "",
       mail: "",
       domicilio: "",
-    });
-    alert(`Alumno '${form.nombre} ${form.apellido}' Agregado Exitosamente!!`)
+      });
+    } else {
+      const agregado = onAdd(form)
+      if(agregado) {
+        alert(`Alumno '${form.nombre} ${form.apellido}' Agregado Exitosamente!!`)
+        if(onClose) onClose();
+        setForm({ 
+        lu: "",
+        nombre: "",
+        apellido: "",
+        curso: "",
+        telefono: "",
+        mail: "",
+        domicilio: "",
+      });
+      }
+      
+    }
+    console.log(form)
+    console.log("el formulario se envio")
   };
 
   return (
-    <Container className='container-product'  style={{}}>
-      <Form onSubmit={handleSubmit} className='product-form'>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Container className='container mt-3'>
+      
+      <Form onSubmit={handleSubmit} className='row justify-content-center '>
+        <div className="col-10 col-sm-8 col-md-6 col-lg-6">
+
+          {modo !== "editar" &&(
+            <Form.Group className="mb-3" controlId="formBasicEmail">
           <InputGroup className="mb-3">
             <Form.Control
               name="lu"
@@ -50,9 +80,18 @@ const AddAlumno = ({ onAdd }) => {
               value={form.lu}
               onChange={handleChange}
               required
+              pattern="^\d+$"
+              title="Solo se aceptan Numeros!"
             />
           </InputGroup>
         </Form.Group>
+          )}
+        {modo === "editar" &&(
+          <div className="mb-3">
+            <strong>LU:</strong> {form.lu}
+          </div>
+        )}
+        
 
         <Form.Group className="mb-3" controlId="formNombre">
           <InputGroup className="mb-3">
@@ -62,6 +101,8 @@ const AddAlumno = ({ onAdd }) => {
               value={form.nombre}
               onChange={handleChange}
               required
+              pattern="^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$"
+              title="Solo se aceptar Letras!"
             />
           </InputGroup>
         </Form.Group>
@@ -74,6 +115,8 @@ const AddAlumno = ({ onAdd }) => {
               value={form.apellido}
               onChange={handleChange}
               required
+              pattern="^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$"
+              title="Solo se aceptar Letras!"
             />
           </InputGroup>
         </Form.Group>
@@ -86,6 +129,8 @@ const AddAlumno = ({ onAdd }) => {
               value={form.curso}
               onChange={handleChange}
               required
+              pattern="^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$"
+              title="Solo se aceptar Letras!"
             />
           </InputGroup>
         </Form.Group>
@@ -98,6 +143,8 @@ const AddAlumno = ({ onAdd }) => {
               value={form.telefono}
               onChange={handleChange}
               required
+              pattern="^\d+$"
+              title="Solo números"
             />
           </InputGroup>
         </Form.Group>
@@ -105,6 +152,7 @@ const AddAlumno = ({ onAdd }) => {
         <Form.Group className="mb-3" controlId="formMail">
           <InputGroup className="mb-3">
             <Form.Control
+            type="email"
               name="mail"
               placeholder="Mail"
               value={form.mail}
@@ -122,12 +170,18 @@ const AddAlumno = ({ onAdd }) => {
               value={form.domicilio}
               onChange={handleChange}
               required
+              pattern="^(?=.*[A-Za-zÁÉÍÓÚáéíóúÑñ])(?=.*\d)[A-Za-zÁÉÍÓÚáéíóúÑñ0-9\s\.,º\-#]+$"
+              title="La dirección debe contener letras y números"
             />
           </InputGroup>
         </Form.Group>
-        <Button type="submit">Agregar Alumno</Button >
+        <Button type="submit">
+          {modo === "editar" ? "Editar Alumno" : "Agregar Alumno"}
+        </Button >
+        </div>
       </Form>
     </Container>
+    
   );
 };
 
